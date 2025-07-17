c041dc0faaf513de2e2def253f2f66344ad54b86
logieinc <140330676+logieinc@users.noreply.github.com>
2025-07-17 13:50:05 -0300

    first commit

---
+++ old_app/app/src/main/java/com/example/curso_app/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.curso_app
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.curso_app.databinding.ActivityMainBinding
+import com.google.firebase.Firebase
+import com.google.firebase.crashlytics.crashlytics
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        Firebase.crashlytics.log("Entré a MainActivity")
+        Firebase.crashlytics.setUserId("usuario123")
+        Firebase.crashlytics.recordException(Exception("Error manual de prueba"))
+    }
+}
