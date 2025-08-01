637d69602fa424ba575f217fb2b3afd53a67877a
rdlimon <rincon.del.limon@gmail.com>
2025-08-01 14:24:01 -0300

    Clase 20250801 pausa

--- app/src/main/java/com/example/curso_final_app/MainActivity.kt
+++ app/src/main/java/com/example/curso_final_app/MainActivity.kt
@@ -35,8 +35,8 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-               // R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-                R.id.navigation_post, R.id.navigation_dashboard, R.id.navigation_notifications
+               //R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+               R.id.navigation_post, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
---
+++ app/src/main/java/com/example/curso_final_app/ui/post/PostAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.curso_final_app.ui.post
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.curso_final_app.data.model.Post
+import com.example.curso_final_app.R
+
+class PostAdapter (
+    private var posts: List<Post>,
+    private val onEdit: (Post) -> Unit,
+    private val onDelete: (Post) -> Unit
+) : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
+    class PostViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvTitle: TextView = view.findViewById(R.id.postTitle)
+        val tvBody: TextView = view.findViewById(R.id.postBody)
+        val btnEdit: Button = view.findViewById(R.id.postBtnEdit)
+        val btnDelete: Button = view.findViewById(R.id.postBtnDelete)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_post, parent, false)
+        return PostViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = posts.size
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int){
+        val post = posts[position]
+        holder.tvTitle.text = post.title
+        holder.tvBody.text = post.body
+
+        holder.btnEdit.setOnClickListener { onEdit(post) }
+        holder.btnDelete.setOnClickListener { onDelete(post) }
+    }
+}
